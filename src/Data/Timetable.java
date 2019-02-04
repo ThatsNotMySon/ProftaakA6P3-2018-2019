f360559560fa805ee36e127225a4013bef795a5c
Tom <tomkaas74@gmail.com>
2019-02-04 11:36:59 +0100

    Merged data/Master

--- src/Data/Timetable.java
+++ src/Data/Timetable.java
@@ -1,4 +1,33 @@
 package Data;
 
-public class Timetable {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Timetable implements Serializable {
+
+    private ArrayList<Event> events;
+
+    public Timetable(){
+        this.events = new ArrayList<Event>();
+    }
+
+    public void addEvent(Event event){
+        this.events.add(event);
+    }
+
+    public ArrayList<Room> getAllRooms(){
+        ArrayList<Room> rooms = new ArrayList<>();
+        for (Event event : events){
+            rooms.add(event.getRoom());
+        }
+        return rooms;
+    }
+
+    public void loadTimetableFromFila(String filepath){
+
+    }
+
+    public void saveTimetableToFile(String filepath){
+
+    }
 }
