c648d0f50c61ebd5716b7f788349d4d0f40f5aad
Marleen <mroubos2@avans.nl>
2019-02-04 10:53:40 +0100

    Window met menu balk

--- src/GUI/GUIMain.java
+++ src/GUI/GUIMain.java
@@ -1,4 +1,39 @@
 package GUI;
 
-public class GUIMain {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GUIMain  extends Application {
+
+@Override
+public void start(Stage primaryStage){
+
+
+        MenuBar menubar=new MenuBar();
+        VBox vBox=new VBox(menubar);
+        Scene scene=new Scene(vBox,960,600);
+
+        Menu file=new Menu("File");
+        Menu rooms=new Menu("Rooms");
+        Menu group=new Menu("Group");
+        Menu lesson=new Menu("Lesson");
+        Menu simulation=new Menu("Simulation");
+
+        menubar.getMenus().add(file);
+        menubar.getMenus().add(rooms);
+        menubar.getMenus().add(group);
+        menubar.getMenus().add(lesson);
+        menubar.getMenus().add(simulation);
+
+
+        primaryStage.setScene(scene);
+
+
+        primaryStage.setTitle("Agenda");
+        primaryStage.show();
+        }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import GUI.GUIMain;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+import static javafx.application.Application.launch;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(GUIMain.class);
     }
+
+
+
+
 }
