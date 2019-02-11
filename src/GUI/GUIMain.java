c0245dafe2ae03e3f222a56852f49815d2bcbfc5
Marleen <mroubos2@avans.nl>
2019-02-11 14:26:54 +0100

    Rooster blok kloppend met data van lokalen (hardcode)

--- src/GUI/GUIMain.java
+++ src/GUI/GUIMain.java
@@ -59,10 +59,11 @@ public void start(Stage primaryStage){
 
     public void draw(FXGraphics2D graphics) {
     int time = 300;
-    int pixelVertical = (int)this.canvas.getHeight()/26;
+    int pixelVertical = (int)this.canvas.getHeight()/27;
     int pixelHorizontal = 0;
     int hours = 0;
     int minuten = 0;
+
     Timetable timetable = new Timetable();
         timetable.addLesson(new Lesson(LocalTime.of(9,30), 60, "Johan Talboom", "JavaFX", new Room("ld120", 15), new Group("A6", 6)));
         timetable.addLesson(new Lesson(LocalTime.of(10,30), 60, "Johan Talboom", "JavaFX", new Room("ld121", 15), new Group("A5", 6)));
@@ -70,21 +71,26 @@ public void start(Stage primaryStage){
 
 
 
-        for (int i = 0; i < timetable.getAllRooms().size() ; i++) {
+        graphics.draw(new Line2D.Double(50, 0, 50, this.canvas.getHeight()));
+
+        int whidthRoom = (int) (this.canvas.getWidth() - 50) / timetable.getAllRooms().size();
+
+        for (int i = 0; i < timetable.getAllRooms().size()-1 ; i++) {
    //         graphics.draw(new Line2D.Double(i,0,i,900));
-            pixelHorizontal = i* ((int)(this.canvas.getWidth()/timetable.getAllRooms().size())+1);
-            graphics.drawString(timetable.getAllRooms().get(i).getName(), pixelHorizontal+50, pixelVertical);
-            graphics.draw(new Line2D.Double(pixelHorizontal, 0,  pixelHorizontal, 900));
+            pixelHorizontal = 50 + whidthRoom + whidthRoom*i;
+            graphics.draw(new Line2D.Double(pixelHorizontal, 0,  pixelHorizontal, this.canvas.getHeight()));
+        }
+
+
+        for (int i = 0; i < timetable.getAllRooms().size(); i++) {
+            graphics.drawString(timetable.getAllRooms().get(i).getName(), 75 + whidthRoom * i, pixelVertical-10);
         }
 
 
 
-        for (int i = 0; i <26; i++) {
-            time = time + 30;
-            pixelVertical = i*((int)this.canvas.getHeight()/26); //gedeeld door de maximum i
-
-
-
+        for (int i = 0; i <27-1; i++) {
+            time += 30;
+            pixelVertical = ((int)this.canvas.getHeight()/27) + i*((int)this.canvas.getHeight()/27); //gedeeld door de maximum i
 
             if (time%60 == 0){
                 hours = time/60;
@@ -94,8 +100,8 @@ public void start(Stage primaryStage){
                 minuten = 30;
             }
 
-            graphics.draw(new Line2D.Double(0,pixelVertical,1200,pixelVertical));
-            graphics.drawString(LocalTime.of(hours, minuten).toString(), 0, pixelVertical);
+            graphics.draw(new Line2D.Double(0,pixelVertical,this.canvas.getWidth(),pixelVertical));
+            graphics.drawString(LocalTime.of(hours, minuten).toString(), 0, pixelVertical+23);
         }
     }
 }
